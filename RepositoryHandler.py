2acf19c15dd44d0647222f8499c88a370c4a7de0
FerrumBrain <ferrumnote@LAPTOP-BSJ85LTC.localdomain>
2024-04-25 17:46:22 +0200

    Changed criteria for files

--- RepositoryHandler.py
+++ RepositoryHandler.py
@@ -30,7 +30,8 @@ class _RepositoryHandler:
         with open(json_path, 'r') as settings_file:
             args = json.load(settings_file)
         self.args = args["statistics"]
-        self.sufficiency_for_considering = args["sufficiency_for_considering"]
+        self.sufficiency_for_considering_contributors = args["sufficiency_for_considering_contributors"]
+        self.sufficiency_for_considering_files = args["sufficiency_for_considering_files"]
 
         self.authors_by_files = {}
         self.authors_by_modules = {}
@@ -73,7 +74,7 @@ class _RepositoryHandler:
     # 3) Subgroups with the most number of points are desired ones
     def _get_groups_of_developers_files(self) -> List[tuple]:
         top_authors_by_files = {file: list(map(lambda n_author: n_author[1], list(
-            filter(lambda n_author: n_author[0] >= self.sufficiency_for_considering * max(
+            filter(lambda n_author: n_author[0] >= self.sufficiency_for_considering_contributors * max(
                 self.authors_by_files[file].values()),
                    [(self.authors_by_files[file][author], author) for author in self.authors_by_files[file].keys()]))))
                                 for file in self.authors_by_files.keys()}
@@ -90,7 +91,7 @@ class _RepositoryHandler:
 
     def _get_groups_of_developers_modules(self) -> List[tuple]:
         top_authors_by_modules = {module: list(map(lambda n_author: n_author[1], list(
-            filter(lambda n_author: n_author[0] >= self.sufficiency_for_considering * max(
+            filter(lambda n_author: n_author[0] >= self.sufficiency_for_considering_contributors * max(
                 self.authors_by_modules[module].values()),
                    [(self.authors_by_modules[module][author], author) for author in
                     self.authors_by_modules[module].keys()]))))
@@ -111,10 +112,8 @@ class _RepositoryHandler:
         res = {subset: len(list(
             filter(lambda files_list: len([f for f in subset if f in files_list]) == len(subset) and len(subset) > 1,
                    self.files_by_commits))) for subset in _powerset(files)}
-        sorted_values = sorted(list(set(res.values())))
-        top5_value = sorted_values[
-            -5 if len(sorted_values) >= 5 else 1]  # Not 0 so we can avoid empty set and one-element sets
-        return list(filter(lambda subset: res[subset] >= top5_value, res.keys()))
+        a =  list(filter(lambda subset: res[subset] >= self.sufficiency_for_considering_files * max(res.values()), res.keys()))
+        return a
 
     def get_statistics(self) -> Dict:
         difference = set(self.args) - set(self._collect_statistics.keys())
